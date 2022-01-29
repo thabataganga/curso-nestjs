59c1fcee9f0c1f937d632ceb2924e49ed30ea252
thabataganga <ganga.thabata@gmail.com>
2022-01-29 13:11:51 -0300

    controllador trabalhando com produtos

--- produtos-api/src/produtos.controller.ts
+++ produtos-api/src/produtos.controller.ts
@@ -1,32 +1,37 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from './produto.model'
 
 @Controller('produtos')
 export class ProdutosController {
+    produtos: Produto[] = [
+        new Produto("LIV001", "Livro X", 29.90),
+        new Produto("LIV002", "Livro Inciante", 15.90),
+        new Produto("LIV003", "Livro Bla bla ", 12.90)
+    ]
 
     @Get()
-    obterTodos(): string {
-        return 'Lista todos os produtos';
+    obterTodos(): Produto[] {
+        return this.produtos;
     }
 
     @Get(':id')
-    obterUm(@Param() params): string {
-        return `Retorna os dados do produto ${params.id}`;
+    obterUm(@Param() params): Produto {
+        return this.produtos[params.id];
     }
 
     @Post()
-    criar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto criado';
+    criar(@Body() produto: Produto) {
+        produto.id = 100;
+        this.produtos.push(produto);
     }
 
     @Put()
-    alterar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto atualizado';
+    alterar(@Body() produto: Produto): Produto {
+        return produto;
     }
 
     @Delete(':id')
-    apagar(@Param() params): string {
-        return `Apaga o produto ${params.id}`;
+    apagar(@Param() params) {
+        this.produtos.pop();
     }
 }
